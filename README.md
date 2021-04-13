a5115716ad776a00ff2578d0daebbda2f99569b1
a852203465 <852203465@qq.com>
2021-04-13 09:27:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# 安装 docker，docker-compose
+
+ - 将所有文件上传服务器
+ - 切换服务器root权限
+ - 修改 install.sh 权限，命令：chmod +x install.sh
+ - 执行 ./install.sh 即可
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+set -e
+
+# 获取当前目录
+DIR="$(cd "$(dirname "$0")" && pwd)"
+source $DIR/common.sh
+
+set +o noglob
+
+function install_docker {
+
+  # 解压文件
+  tar -zxvf $DIR/docker-20.10.1.tgz
+
+  # 复制文件
+  sudo cp -f $DIR/docker/* /usr/bin/
+
+  # 将docker注册为service
+  sudo cp -f $DIR/docker.service /etc/systemd/system/docker.service
+
+  # 修改权限 启动Docker
+  chmod +x /etc/systemd/system/docker.service
+
+  # 设置daemon.json
+  sudo cp -f $DIR/daemon.json  /etc/docker/daemon.json
+
+  systemctl daemon-reload
+  systemctl start docker			#启动Docker
+  systemctl enable docker.service			#设置开机自启
+}
+
+function restart_docker {
+  systemctl daemon-reload
+  systemctl restart docker
+}
+
+function install_dockercompose {
+  sudo cp -f $DIR/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+}
+
+# 安装 docker, docker-compose
+install_docker
+install_dockercompose
+
+# 验证 docker, docker-compose
+check_docker
+check_dockercompose
+
+# 重启 docker
+restart_docker
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
